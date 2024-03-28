b5d98138afac1269f507f8b7acf077cad439c82e
Jeen Namkung <jnamkung@my.bcit.ca>
2024-03-28 10:44:36 -0700

    change user schema and add update, delete user

--- pages/api/users/[userId]/index.js
+++ pages/api/users/[userId]/index.js
@@ -1,4 +1,4 @@
-import { getUserById } from '../../../../lib/prisma/users'
+import { getUserById, deleteUserById, updateUserById } from '../../../../lib/prisma/users'
 
 const handler = async (req, res) => {
   if (req.method === 'GET') {
@@ -12,7 +12,30 @@ const handler = async (req, res) => {
     }
   }
 
-  res.setHeader('Allow', ['GET'])
+  if(req.method === 'DELETE') {
+    try {
+      const { userId } = req.query
+      const { user, error } = await deleteUserById(userId)
+      if (error) throw new Error(error)
+      return res.status(200).json({ message: 'User deleted' })
+    } catch (error) {
+      return res.status(500).json({ error: error.message })
+    }
+  }
+
+  if(req.method === 'PUT') {
+    try {
+      const { userId } = req.query
+      const dataToUpdate = req.body
+      const { user, error } = await updateUserById(userId, dataToUpdate)
+      if (error) throw new Error(error)
+      return res.status(200).json({ user })
+    } catch (error) {
+      return res.status(500).json({ error: error.message })
+    }
+  }
+
+  res.setHeader('Allow', ['GET', 'DELETE', 'PUT'])
   res.status(425).end(`Method ${req.method} is not allowed.`)
 }
 
