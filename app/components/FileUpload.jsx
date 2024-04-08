cdb85bffae20c9db0939ad2f39c1ae21f181ab60
ys421 <ychung47@my.bcit.ca>
2024-04-08 02:05:41 -0700

    [YVR-Server-65] Extract zone code from csv file

--- app/components/FileUpload.jsx
+++ app/components/FileUpload.jsx
@@ -10,6 +10,7 @@ const fileTypes = ["csv"]
 const FileUpload = () => {
   const [file, setFile] = useState(null);
   const [extractedZoneCode, setExtractedZoneCode] = useState(null);
+  const [isCodeRecognized, setIsCodeRecognized] = useState(true);
 
   const handleChange = (file) => {
     Array.from(file)
@@ -23,9 +24,10 @@ const FileUpload = () => {
           console.log(err);
         } else {
           console.log(data);
+          //Zone Code List : Additional list of zone codes will be added manually
           console.log(calculateHourlyAverages(data))
 
-          //Zone Code List : Additional list of zone codes will be added manually
+          //Extracting Zone Code from the data
           const zoneCodeList = [
             "ITB_VAV_A42_W3327_02", 
             "ITB_VAV_A43_W3325_01_01", 
@@ -36,13 +38,14 @@ const FileUpload = () => {
             "ITB_VAV_A42_W3338_01"
           ];
 
-          //Extracting Zone Code from the data
+          // 구역 코드 추출
           const columns = Object.keys(data[0]);
           const matchingZoneCode = zoneCodeList.find(zoneCode => 
             columns.some(column => column.includes(zoneCode))
           );
 
           setExtractedZoneCode(matchingZoneCode);
+          setIsCodeRecognized(!!matchingZoneCode);
         }
       });
     });
@@ -61,6 +64,11 @@ const FileUpload = () => {
           <div style={{ marginTop: '20px', padding: '10px', border: '1px solid #ccc', borderRadius: '5px', backgroundColor: '#f9f9f9' }}>
             <h3 style={{ margin: '0 0 10px 0' }}>Extracted Zone Code:</h3>
             <p style={{ margin: '0' }}>{extractedZoneCode}</p>
+          </div>
+        )}
+        {!isCodeRecognized && (
+          <div style={{ marginTop: '20px', padding: '10px', border: '1px solid #ccc', borderRadius: '5px', backgroundColor: '#f9f9f9' }}>
+            <p style={{ margin: '0', color: 'red' }}>Unrecognized area code</p>
           </div>
         )}
       </div>
