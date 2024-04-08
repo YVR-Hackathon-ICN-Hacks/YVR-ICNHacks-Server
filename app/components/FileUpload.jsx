96b21bf95b27da1e2a6bc9e5de958a840cccceea
ys421 <ychung47@my.bcit.ca>
2024-04-08 01:59:20 -0700

    [YVR-Server-65] Extract zone code from csv file

--- app/components/FileUpload.jsx
+++ app/components/FileUpload.jsx
@@ -9,10 +9,12 @@ const fileTypes = ["csv"]
 
 const FileUpload = () => {
   const [file, setFile] = useState(null);
+  const [extractedZoneCode, setExtractedZoneCode] = useState(null);
+
   const handleChange = (file) => {
     Array.from(file)
     .filter((file) => file.type === "text/csv")
-    .forEach( async (file) => {
+    .forEach(async (file) => {
 
       const text = await file.text();
       
@@ -21,15 +23,30 @@ const FileUpload = () => {
           console.log(err);
         } else {
           console.log(data);
-          //TODO YS's algorithm will be implemented here
           console.log(calculateHourlyAverages(data))
 
+          //Zone Code List : Additional list of zone codes will be added manually
+          const zoneCodeList = [
+            "ITB_VAV_A42_W3327_02", 
+            "ITB_VAV_A43_W3325_01_01", 
+            "ITBFC3037_1", 
+            "ITB_VAV_A43_W3325_01_01", 
+            "ITB_VAV_A42_W3325_02", 
+            "ITB_VAV_A42_W3327_01", 
+            "ITB_VAV_A42_W3338_01"
+          ];
 
+          //Extracting Zone Code from the data
+          const columns = Object.keys(data[0]);
+          const matchingZoneCode = zoneCodeList.find(zoneCode => 
+            columns.some(column => column.includes(zoneCode))
+          );
+
+          setExtractedZoneCode(matchingZoneCode);
         }
       });
     });
   };
-  
 
   return (
     <div>
@@ -39,7 +56,14 @@ const FileUpload = () => {
           name="file"
           types={fileTypes}
         />
-
+      <div>
+        {extractedZoneCode && (
+          <div style={{ marginTop: '20px', padding: '10px', border: '1px solid #ccc', borderRadius: '5px', backgroundColor: '#f9f9f9' }}>
+            <h3 style={{ margin: '0 0 10px 0' }}>Extracted Zone Code:</h3>
+            <p style={{ margin: '0' }}>{extractedZoneCode}</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
