8a1a44d767f60b0f8366785984e509180068ac8a
Jasper Oh <ohyj0906@gmail.com>
2024-04-08 00:26:12 -0700

    [YVR-Server-61] File Upload drag & Drop with csvToJson

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,12 @@
+'use client'
+import React from "react";
+import FileUpload from "./components/FileUpload";
+
 export default function UploadPage() {
   return (
     <div>
       <h1>Upload Page</h1>
+      <FileUpload/>
     </div>
   );
 }
