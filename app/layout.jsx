e7a3835a57033dc6fed856722c7f97f9bbbf0ced
ys421 <ychung47@my.bcit.ca>
2024-04-11 03:33:03 -0700

    [YVR-Server-68] Added logo

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,14 @@
+import Head from 'next/head';
+
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Admin</title>
+        <title>YVR-HACKS</title>
+        <link rel="icon" type="image/png" sizes="32x32" href="image/logo.png" />
       </head>
       <body>
         <div>
