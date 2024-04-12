cd60cd290e42f8da524403a5ac295da4251a4098
Jasper Oh <ohyj0906@gmail.com>
2024-04-12 01:28:27 -0700

    yvr-server-80 server change

---
+++ pages/api/pushToken/index.js
@@ -0,0 +1,32 @@
+import { createPushToken, getAllPushToken } from '@/lib/prisma/pushToken'
+import CustomError from '@/lib/errors/CustomError'
+import NextCors from 'nextjs-cors'
+import globalExceptionHandler from '@/lib/utils/globalExceptionHandler'
+
+const handler = async (req, res) => {
+  await NextCors(req, res, {
+    methods: ['GET', 'PUT', 'DELETE'],
+    origin: '*',
+    optionsSuccessStatus: 200,
+  })
+
+  try {
+    if (req.method === 'GET') {
+      const { pushToken } = await getAllPushToken()
+      return res.status(200).json({ pushToken })
+    }
+
+    if (req.method === 'POST') {
+      const newPushToken = req.body
+      const { pushToken } = await createPushToken(newPushToken)
+      return res.status(200).json({ pushToken })
+    }
+
+    res.setHeader('Allow', ['GET', 'POST'])
+    throw new CustomError(405)
+  } catch (error) {
+    globalExceptionHandler(error, req, res)
+  }
+}
+
+export default handler;
